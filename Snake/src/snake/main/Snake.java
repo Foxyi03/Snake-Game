9c60b8eaa74486cee0dd31c1adae876d4af2350c
Foxyi03 <patrick.villiet@gmail.com>
2024-05-08 20:51:18 +0200

    problem

--- Snake/src/snake/main/Snake.java
+++ Snake/src/snake/main/Snake.java
@@ -85,9 +85,9 @@ public class Snake {
     }
     
     public void draw(Graphics g){ //draws the apple and the snake
-        if(inField){
-            g.drawImage(apple, appleX, appleY, (ImageObserver) this);
-            g.drawImage(bread,breadX, breadY,(ImageObserver)this);
+        if(!inField){
+            g.drawImage(apple, appleX, appleY, this); //snake cannot be converted to ImageObserver
+            g.drawImage(bread,breadX, breadY,this);
             
             for(int i = 0; i < parts; i++){
                 if(i ==0){
